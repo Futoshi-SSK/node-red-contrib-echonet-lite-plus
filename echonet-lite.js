25a1e99cfd496bfb29e26e5f139fa942e8a9ce83
Futoshi-SSK <sasaki.futoshi@outlook.jp>
2026-01-28 21:07:53 +0900

    Interim Version

--- echonet-lite.js
+++ echonet-lite.js
@@ -5,7 +5,7 @@ module.exports = function(RED) {
     function EchonetLiteNode(n) {
         RED.nodes.createNode(this, n);
         const node = this;
-        // TIPS: UI側のIP指定が location または ip であることを考慮
+        // プロパティ名は環境によって location だったり ip だったりするため両対応
         this.ip = n.ip || n.host || n.location || n.address; 
         const el = new echonet({ 'lang': 'ja', 'type': 'lan' });
 
@@ -15,14 +15,13 @@ module.exports = function(RED) {
             const epc = msg.epc || "E2";
             const epc_num = parseInt(epc, 16);
 
-            // 共通のレスポンス処理
             const handleResponse = (err, res, source) => {
                 if (err) {
                     node.status({fill:"red", shape:"ring", text: source + " error"});
                     msg.payload = "TIMEOUT_ERROR";
                 } else {
                     try {
-                        // getPropertyValue と el.send でデータの深さが異なるのを吸収
+                        // ライブラリのメソッドによってレスポンス構造が違うのを吸収
                         const rawData = res.message ? res.message.data.edt : res.detail.property[0].edt;
                         msg.payload = Buffer.from(rawData).toString('hex').toUpperCase();
                         node.status({fill:"green", shape:"dot", text:"OK(" + source + "): " + msg.payload});
@@ -33,7 +32,7 @@ module.exports = function(RED) {
                 node.send(msg);
             };
 
-            // --- 1. 設定系 (SetC: 0x61) ---
+            // --- A. 設定系 (SetC: 0x61) ---
             if (msg.set_value !== undefined && msg.set_value !== null) {
                 node.status({fill:"blue", shape:"dot", text:"setting..."});
                 const props = [{ 'epc': epc_num, 'edt': Buffer.from(msg.set_value.toString(), 'hex') }];
@@ -41,14 +40,15 @@ module.exports = function(RED) {
                     handleResponse(err, res, "set");
                 });
             } 
-            // --- 2. 照会系 (Get: 0x62) ---
+            // --- B. 照会系 (Get: 0x62) ---
             else {
                 node.status({fill:"blue", shape:"dot", text:"getting..."});
+                // 1. まずは標準の方法で試す
                 el.getPropertyValue(address, deoj, epc, (err, res) => {
-                    // 「Not supported」エラーが出た場合のみ、辞書バイパスを実行
                     if (err && err.message.includes("not supported")) {
+                        // 2. 「辞書にない」と言われたら、辞書を無視する el.send で強制実行
                         node.status({fill:"yellow", shape:"dot", text:"bypassing dict..."});
-                        // Get要求時は 'edt' キーを含めないことで Error (2) を回避
+                        // Get時は 'edt' プロパティ自体を消すのが Error(2) 回避のコツ
                         const props = [{ 'epc': epc_num }]; 
                         el.send(address, [0x05, 0xFF, 0x01], deoj, 0x62, props, (errRaw, resRaw) => {
                             handleResponse(errRaw, resRaw, "raw-get");
